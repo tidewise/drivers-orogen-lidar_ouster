3eac5056888c3cd3849437b662e651781799ed9b
Afonso da Fonseca Braga <afonso.braga@engenharia.ufjf.br>
2023-01-04 15:01:11 -0300

    initial commit by rock-create-orogen

---
+++ lidar_ousterTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef lidar_ouster_TYPES_HPP
+#define lidar_ouster_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace lidar_ouster {
+}
+
+#endif
+
